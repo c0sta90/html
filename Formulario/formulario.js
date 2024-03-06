9e3d46105759e9d9e88f6852a985aa40daa8e01a
C0sta <filipe.rocha.costa12@gmail.com>
2024-03-06 11:56:51 -0100

    Update

--- Formulario/formulario.js
+++ Formulario/formulario.js
@@ -3,8 +3,13 @@ Codigo principal da framework jquerry
 */
 $(document).ready(function(){
     console.log("Documento está carregado");
+    $("#load").hide();
     
     $("input, textarea").on("focusin focusout", function(event){ /* textarea para dar select ao text */
         $(this).toggleClass("border-form");
     });
+    $("form").on("submit", function(event){
+        event.preventDefault(); /*evita fazer o default que é carregar a pagina*/
+    $("#load").show();
+    })
 });
